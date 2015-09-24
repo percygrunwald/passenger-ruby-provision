1cc7a5950a6f2dfdfb0c48004a73e772dbdeb11a
Percy Grunwald <percy.grunwald@gmail.com>
2015-09-24 10:15:09 +0800

    Removes unnecessary installation steps in file 2

--- 2-install-rails-test.sh
+++ 2-install-rails-test.sh
@@ -25,20 +25,6 @@ rvm use ruby
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
 
-# Install nodejs
-sudo apt-get install -y nodejs && sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
-
-# Install Passenger PGP key and add HTTPS support for APT
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates
-
-# Add Passenger APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
-
-# Install Passenger + Nginx + git
-sudo apt-get install -y nginx-extras passenger git
-
 # Set proper paths in nginx and restart
 pass_root=`passenger-config --root` && sudo sed -i "s,\# passenger_root .*;,passenger_root $pass_root;,g" /etc/nginx/nginx.conf
 sudo sed -i 's,# passenger_ruby,passenger_ruby,g' /etc/nginx/nginx.conf
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # passenger-ruby-provision
+A shell script that installs Phusion Passenger and RVM. Run the script on a cloud server and be ready to deploy.
