e60a4f070dfe1ddec857a4d89067ba5762a41cfc
Percy Grunwald <percy.grunwald@gmail.com>
2016-02-03 22:50:28 +0800

    Adds "sudo" to lets encrypt reminder

--- 3-deploy-app.sh
+++ 3-deploy-app.sh
@@ -17,7 +17,7 @@ cp .env.example .env
 RAKE_SECRET=`bundle exec rake secret`
 echo "SECRET_KEY_BASE=$RAKE_SECRET" >> .env
 nano .env
-RAILS_ENV=production bundle exec rake assets:precompile db:migrate
+RAILS_ENV=production bundle exec rake assets:precompile db:migrate db:seed
 
 # Add post-receive hook for git
 cat << EOF | tee /home/$INPUT_USERNAME/$INPUT_USERNAME.git/hooks/post-receive
@@ -27,7 +27,7 @@ eval "\$(rbenv init -)"
 git --work-tree=/var/www/$INPUT_USERNAME --git-dir=/home/$INPUT_USERNAME/$INPUT_USERNAME.git checkout master -f
 (cd /var/www/$INPUT_USERNAME && \\
 RAILS_ENV=production bundle install --path vendor --deployment --without development test && \\
-RAILS_ENV=production bundle exec rake assets:precompile db:migrate && \\
+RAILS_ENV=production bundle exec rake assets:precompile assets:clean db:migrate && \\
 chmod 700 config db && chmod 600 config/database.yml config/secrets.yml)
 passenger-config restart-app /var/www/$INPUT_USERNAME
 EOF
